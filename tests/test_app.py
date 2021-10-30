dcbbe516cd6f187a7381ab5bb26f06382123c229
Kuan-Chun (Jean) Lin <jeanlinkcl@gmail.com>
2021-10-30 16:00:53 -0400

    clean up and re-format the code

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,6 @@
 import pytest
-import os
-from pathlib import Path
 import json
-
+from pathlib import Path
 from project.app import app, db
 
 TEST_DB = "test.db"
@@ -75,11 +73,6 @@ def test_messages(client):
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
 
-def test_delete_message(client):
-    """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
-    data = json.loads(rv.data)
-    assert data["status"] == 1
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
