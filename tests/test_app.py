acca002210953cab0fda9ef7ffebb7932bd7af41
Kuan-Chun (Jean) Lin <jeanlinkcl@gmail.com>
2021-10-30 13:38:43 -0400

    set up database

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
-from project.app import app
+from pathlib import Path
+from project.app import app, init_db
 
 
 def test_index():
@@ -6,3 +7,7 @@ def test_index():
     response = tester.get("/", content_type="html/text")
     assert response.status_code == 200
     assert response.data == b"Hello, World!"
+
+def test_database():
+    init_db()
+    assert Path("flaskr.db").is_file()
